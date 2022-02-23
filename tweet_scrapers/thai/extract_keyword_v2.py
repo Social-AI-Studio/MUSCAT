63d160d80755e69a1350208ee257bed15aabefb3
dangn511 <dangn511@gmail.com>
2022-02-23 08:41:16 +0800

    update thai scraping scripts

---
+++ tweet_scrapers/thai/extract_keyword_v2.py
@@ -0,0 +1,78 @@
+from keybert import KeyBERT
+import os
+import json
+import pandas as pd
+import html
+from translate_utils import *
+
+keywords_list = []
+# kw_model = KeyBERT(model="distiluse-base-multilingual-cased-v2")
+kw_model = KeyBERT()
+kw_model_multi = KeyBERT(model="distiluse-base-multilingual-cased-v2")
+
+rootdir = "thai_articles"
+for file in os.listdir(rootdir):
+    with open(os.path.join(rootdir, file), 'r') as f:
+        content = json.load(f)
+        title = content['title']
+        lang = content['language']
+
+        if lang == "en":
+        # print(title)
+            keywords = kw_model.extract_keywords(title, stop_words="english", keyphrase_ngram_range=(1, 1), top_n=5)
+            sorted_keywords = sorted(keywords, key=lambda tup: tup[1], reverse=True)
+            # print(sorted_keywords)
+            selected_keywords = [i[0] for i in keywords]
+
+            content["keywords"] = selected_keywords
+            path_to_save = rootdir+"_with_keywords"
+            if not os.path.exists(path_to_save):
+                os.mkdir(path_to_save)
+
+            with open(os.path.join(path_to_save, file), 'w') as handle:
+                json.dump(content, handle, indent=4)
+
+        elif lang == "th":
+            translated_title = translate_text_with_source(source="th", target="en", text=title)
+            content["title_eng"] = translated_title
+            # print(title)
+            # print(translated_title)
+            keywords_eng = kw_model.extract_keywords(translated_title, keyphrase_ngram_range=(1, 1), top_n=5)
+            # print(keywords_thai)
+            selected_keywords = [i[0] for i in keywords_eng]
+            content["keywords_eng"] = selected_keywords
+            keywords_thai = [translate_text_with_source(source="en", target="th", text=word) for word in selected_keywords]
+            content["keywords"] = keywords_thai
+            # keywords = kw_model.extract_keywords(translated_title, stop_words="english", keyphrase_ngram_range=(1, 1), top_n=5)
+            # sorted_keywords = sorted(keywords, key=lambda tup: tup[1], reverse=True)
+            # # print(sorted_keywords)
+            # selected_keywords = [i[0] for i in keywords]
+            # content["keywords_eng"] = selected_keywords
+            # keywords_thai = [translate_text_with_source(source="en", target="vi", text=word) for word in selected_keywords]
+            # content["keywords"] = keywords_thai
+
+            path_to_save = rootdir + "_with_keywords_v2"
+            if not os.path.exists(path_to_save):
+                os.mkdir(path_to_save)
+
+            with open(os.path.join(path_to_save, file), 'w') as handle:
+                json.dump(content, handle, indent=4)
+
+
+# df = pd.read_csv("indo_new_trans.csv", encoding='utf-8')
+# print(df)
+#
+# titles_eng = [html.unescape(i) for i in df["Title_Eng"]]
+#
+# for title in titles_eng:
+#     # use unescape to fix the html entities
+#     print(html.unescape(title))
+#     keywords = kw_model.extract_keywords(html.unescape(title), stop_words="english", keyphrase_ngram_range=(1, 1))
+#     # only pick keywords with a confidence score above 0.1
+#     selected_keywords = [i[0] for i in keywords]
+#     print(selected_keywords)
+#     keywords_list.append(selected_keywords)
+#
+# df["keywords"] = keywords_list
+# df.to_csv("indo_with_keywords.csv", index=False)
+
