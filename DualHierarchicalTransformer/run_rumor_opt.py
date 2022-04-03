d2a208628ec22a4cb065b4e34f94c9e88b42c3e0
Md Rabiul Awal <mda219@laksa.usask.ca>
2022-04-03 11:05:53 -0600

    fixed json encoder error

--- DualHierarchicalTransformer/run_rumor_opt.py
+++ DualHierarchicalTransformer/run_rumor_opt.py
@@ -64,6 +64,26 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "to_json"):
+            return self.default(obj.to_json())
+        elif hasattr(obj, "__dict__"):
+            d = dict(
+                (key, value)
+                for key, value in inspect.getmembers(obj)
+                if not key.startswith("__")
+                and not inspect.isabstract(value)
+                and not inspect.isbuiltin(value)
+                and not inspect.isfunction(value)
+                and not inspect.isgenerator(value)
+                and not inspect.isgeneratorfunction(value)
+                and not inspect.ismethod(value)
+                and not inspect.ismethoddescriptor(value)
+                and not inspect.isroutine(value)
+            )
+            return self.default(d)
+        return str(obj)
 
 class InputExample(object):
     """A single training/test example for simple sequence classification."""
@@ -968,6 +988,7 @@ def main():
             json.dump(
                 {"args": vars(args), "eval_result": result},
                 fout,
+                cls=ObjectEncoder,
                 indent=4,
             )
 
